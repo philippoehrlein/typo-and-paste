77dc5f425072e0a17860b92c5ed4c5fd71f1d92f
Johann Schopplich <mail@johannschopplich.com>
2024-11-27 11:07:53 +0100

    chore: consistent spacing

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 use Kirby\Cms\App as Kirby;
 
 Kirby::plugin('philippoehrlein/typo-and-paste', [
-    'translations' => require __DIR__ . '/config/translations.php',
-    'options' => [
-        'characters' => require __DIR__ . '/config/characters.php'
-    ],
-    'api' => require __DIR__ . '/config/api.php',
-    'version' => '1.1.0'
+  'api' => require __DIR__ . '/config/api.php',
+  'translations' => require __DIR__ . '/config/translations.php',
+  'options' => [
+    'characters' => require __DIR__ . '/config/characters.php'
+  ],
+  'version' => '1.1.0'
 ]);
--- playground/public/bootstrap.php
+++ playground/public/bootstrap.php
@@ -5,15 +5,15 @@ $base = dirname(__DIR__);
 require dirname($base) . '/vendor/autoload.php';
 
 $kirby = new \Kirby\Cms\App([
-    'roots' => [
-        'index'    => __DIR__,
-        'base'     => $base,
-        'site'     => $base . '/site',
-        'storage'  => $storage = $base . '/storage',
-        'content'  => $storage . '/content',
-        'accounts' => $storage . '/accounts',
-        'cache'    => $storage . '/cache',
-        'logs'     => $storage . '/logs',
-        'sessions' => $storage . '/sessions'
-    ]
+  'roots' => [
+    'index'    => __DIR__,
+    'base'     => $base,
+    'site'     => $base . '/site',
+    'storage'  => $storage = $base . '/storage',
+    'content'  => $storage . '/content',
+    'accounts' => $storage . '/accounts',
+    'cache'    => $storage . '/cache',
+    'logs'     => $storage . '/logs',
+    'sessions' => $storage . '/sessions'
+  ]
 ]);
